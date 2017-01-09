299b8b58cb2987e9e26aa569e9628ddde8675941
akahard2dj <akahard2dj@naver.com>
2017-01-09 23:39:33 +0900

    Update aes_cipher.py

--- app/models/aes_cipher.py
+++ app/models/aes_cipher.py
@@ -1,11 +1,14 @@
 import binascii
-from Crypto.Cipher import AES
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
 
 class AESCipher:
-    def __init__(self, key, bs=16):
+    def __init__(self, key, iv, bs=16):
         self.__BS = bs
-        self.__key = key
+        self.__key = key.encode()
+        self.__iv = iv.encode()
+        self.__cipher = Cipher(algorithms.AES(self.__key), modes.CBC(self.__iv), backend=default_backend())
 
     def pad(self, s):
         BS = self.__BS
@@ -16,16 +19,16 @@ class AESCipher:
         return s[:-ord(s[len(s)-1:])]
 
     def encrypt(self, data):
+        encryptor = self.__cipher.encryptor()
         data_enc = data.encode()
         raw = self.pad(data_enc)
-        cipher = AES.new(self.__key)
-        enc = cipher.encrypt(raw)
+        enc = encryptor.update(raw)
 
         return binascii.hexlify(enc).decode()
 
     def decrypt(self, data):
+        decryptor = self.__cipher.decryptor()
         enc = binascii.unhexlify(data)
-        cipher = AES.new(self.__key)
-        dec = cipher.decrypt(enc)
+        dec = decryptor.update(enc)
 
         return self.unpad(dec).decode()
