1409c221ae38959d22f696d53c15e645780e3ca5
akahard2dj <akahard2dj@naver.com>
2017-01-10 16:31:14 +0900

    Update post.py

--- app/models/post.py
+++ app/models/post.py
@@ -6,8 +6,6 @@ from app.models.aes_cipher import AESCipher
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql.expression import func, cast
 
-cipher = AESCipher(current_app.config['AES_KEY'])
-
 
 class EncryptedPost(db.Model):
     __tablename__ = 'encrypted_post'
@@ -15,11 +13,22 @@ class EncryptedPost(db.Model):
     enc_post_title = db.Column(db.Text)
     enc_post_body = db.Column(db.Text)
     post_timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
-    user_id = db.Column(db.Integer, db.ForeignKey('encrypted_user.user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('encrypted_users.user_id'))
     read_counts = db.Column(db.Integer, default=0)
+
+    def increase_read_count(self):
+        updated_counts = self.read_counts + 1
+        self.read_counts = updated_counts
+        db.session.add(self)
+
+    def edit_post(self, new_post_title, new_post_body):
+        self.post_title = new_post_title
+        self.post_body = new_post_body
+        db.session.add(self)
 
     @hybrid_property
     def post_title(self):
+        cipher = AESCipher(current_app.config['AES_KEY'])
         return cipher.decrypt(self.enc_post_title)
 
     @post_title.expression
@@ -31,10 +40,12 @@ class EncryptedPost(db.Model):
 
     @post_title.setter
     def post_title(self, title):
+        cipher = AESCipher(current_app.config['AES_KEY'])
         self.enc_post_title = cipher.encrypt(title)
 
     @hybrid_property
     def post_body(self):
+        cipher = AESCipher(current_app.config['AES_KEY'])
         return cipher.decrypt(self.enc_post_body)
 
     @post_body.expression
@@ -46,4 +57,5 @@ class EncryptedPost(db.Model):
 
     @post_body.setter
     def post_body(self, body):
+        cipher = AESCipher(current_app.config['AES_KEY'])
         self.enc_post_body = cipher.encrypt(body)
