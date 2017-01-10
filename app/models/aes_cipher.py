e932b086dc146f62e9fafc4d2cbd636d37c74f08
akahard2dj <akahard2dj@naver.com>
2017-01-10 16:30:32 +0900

    Update aes_cipher.py

--- app/models/aes_cipher.py
+++ app/models/aes_cipher.py
@@ -1,14 +1,16 @@
 import binascii
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+from Crypto.Cipher import AES
+
+#from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+#from cryptography.hazmat.backends import default_backend
 
 
 class AESCipher:
-    def __init__(self, key, iv, bs=16):
+    def __init__(self, key, bs=16):
         self.__BS = bs
-        self.__key = key.encode()
-        self.__iv = iv.encode()
-        self.__cipher = Cipher(algorithms.AES(self.__key), modes.CBC(self.__iv), backend=default_backend())
+        #self.__iv = iv.encode()
+        self.__key = key
+        #self.__cipher = Cipher(algorithms.AES(self.__key), modes.CBC(self.__iv), backend=default_backend())
 
     def pad(self, s):
         BS = self.__BS
@@ -19,16 +21,32 @@ class AESCipher:
         return s[:-ord(s[len(s)-1:])]
 
     def encrypt(self, data):
-        encryptor = self.__cipher.encryptor()
+
         data_enc = data.encode()
         raw = self.pad(data_enc)
-        enc = encryptor.update(raw)
+        cipher = AES.new(self.__key)
+        enc = cipher.encrypt(raw)
 
         return binascii.hexlify(enc).decode()
+        '''
+        data_encode = data.encode()
+
+        raw = self.pad(data_encode)
+        enc = self.__cipher.encryptor().update(raw)
+
+        return binascii.hexlify(enc).decode()
+        '''
 
     def decrypt(self, data):
-        decryptor = self.__cipher.decryptor()
+
         enc = binascii.unhexlify(data)
-        dec = decryptor.update(enc)
+        cipher = AES.new(self.__key)
+        dec = cipher.decrypt(enc)
 
         return self.unpad(dec).decode()
+        '''
+        enc = binascii.unhexlify(data)
+        dec = self.__cipher.decryptor().update(enc)
+
+        return self.unpad(dec).decode()
+        '''
