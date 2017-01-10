5e828f42e7dbd9fd06200d37d6b696a5efed2b94
akahard2dj <akahard2dj@naver.com>
2017-01-10 16:25:43 +0900

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
 from app import create_app, db
 from app.models.user import EncryptedUser as User
+from app.models.post import EncryptedPost as Post
+from app.models.role import Role
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -9,7 +11,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Post=Post, Role=Role)
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
